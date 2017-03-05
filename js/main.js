f8ecdcccdf49b8ee6aaa321492f521563a6ab645
miriampc <miiriampc@gmail.com>
2017-03-05 15:13:34 -0500

    primer commit implementando funcion traductor

---
+++ js/main.js
@@ -0,0 +1,26 @@
+var title=document.getElementById('form-signin-heading');
+var email=document.getElementById('inputEmail');
+var pswd=document.getElementById('inputPassword');
+var check=document.getElementsByTagName('span')[0];
+var btn=document.getElementById('traduce');
+
+function traductor(){
+  title.innerHTML="Por favor inicia sesión";
+  email.placeholder="Correo Electrónico";
+  pswd.placeholder="Contraseña";
+  check.innerHTML="Recordar datos";
+  btn.innerHTML="Iniciar Sesión";
+}
+traductor();
+
+document.getElementById('traduce').onclick = function mostrar(){
+    var imp=document.getElementById('print');
+    imp.innerHTML="<h4>Datos de formulario</h4>"+
+                    "El correo electrónico ingresado es: "+email.value+
+                    "<br>La clave ingresada es: "+pswd.value;
+
+}
+
+document.getElementById('form').onsubmit=function(e){
+  e.preventDefault();
+}
